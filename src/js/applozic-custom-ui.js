9b33f1202669cb4d04968d79679ca332b36987d5
Kaustubh Ekbote <k.ekbote@easternenterprise.com>
2019-03-14 19:04:44 +0530

    Get message by topic and conversation, get all topics, get conversation by topic, etc.

--- src/js/applozic-custom-ui.js
+++ src/js/applozic-custom-ui.js
@@ -1,62 +1,56 @@
 var customApp = {
     apiKey: 'easternenterprise9cfa65d65d6f060a9993ecedb95fa3df',
+    deviceKey: '',
     login: function(userId, password, displayName) {
         var self = this;
-        Applozic.ALApiService.login(
-            {
-                data: {
-                    baseUrl: 'https://apps.applozic.com',
-                    alUser:
-                    {
-                        userId: userId,
-                        password: password,
-                        displayName: displayName,
-                        appVersionCode: 108,
-                        applicationId: self.apiKey,
-                        imageLink: '', //User's profile picture url
-                        email: '', //optional
-                        contactNumber: '', //optional, pass with internationl code eg: +13109097458
-                        topicBox: true
-                    }
-                },
-                success: function(response) {
-                    console.log(response);
-                    var data = {};
-                    data.token = response.token;
-                    data.deviceKey = response.deviceKey;
-                    data.websocketUrl = response.websocketUrl;
-
-                    //Get your App ID from [Applozic Dashboard](https://console.applozic.com/settings/install)
-                    window.Applozic.ALSocket.init(self.apiKey, data, events);
-                },
-                error: function() {
-
+        Applozic.ALApiService.login({
+            data: {
+                baseUrl: 'https://apps.applozic.com',
+                alUser: {
+                    userId: userId,
+                    password: password,
+                    displayName: displayName,
+                    appVersionCode: 108,
+                    applicationId: self.apiKey,
+                    imageLink: '',
+                    email: '',
+                    contactNumber: '',
+                    topicBox: true
                 }
-            }
-        );
+            },
+            success: function(response) {
+                console.log(response);
+                var data = {};
+                data.token = response.token;
+                data.deviceKey = response.deviceKey;
+                data.websocketUrl = response.websocketUrl;
+                self.deviceKey = response.deviceKey;
+                window.Applozic.ALSocket.init(self.apiKey, data, events);
+            },
+            error: function() {}
+        });
     },
     sendMessage: function (type, contentType, message, to) {
         Applozic.ALApiService.sendMessage({
             data: {
                 message: {
-                    "type": type,
-                    "contentType": contentType,
-                    "message": message,
-                    "to": to,
-                    "metadata": {},
-                    "source": 1
+                    'type': type,
+                    'contentType': contentType,
+                    'message': message,
+                    'to': to,
+                    'metadata': {},
+                    'source': 1
                 }
             },
             success: function (response) { console.log(response); },
-            error: function () { }
+            error: function () {}
         });
-
     },
     messageList: function (offset, limit) {
         Applozic.ALApiService.getMessages({
             data: {
-                startIndex:offset,
-                mainPageSize:limit
+                startIndex: offset,
+                mainPageSize: limit
             },
             success: function(response){console.log(response);},
             error: function() {}
@@ -65,11 +59,94 @@ var customApp = {
     getMessageThread: function (userId, offset, limit) {
         Applozic.ALApiService.getMessages({
             data: {
-                startIndex:0,
-                userId:userId,
-                pageSize:30
+                startIndex: offset,
+                userId: userId,
+                pageSize: limit
             },
             success: function(response) {console.log(response);},
-            error: function() {}});
+            error: function() {}
+        });
+    },
+    getConversationId: function (userId, receiverUserId, topicId, topicDetails) {
+        Applozic.ALApiService.getConversationId({
+            data: {
+                'topicId': topicId,
+                'userId': receiverUserId,
+                'status': 'new',
+                'isGroup': false,
+                'topicDetail': JSON.stringify(topicDetails)
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function () {}
+        });
+    },
+    sendMessageInConversation: function (userId, message, conversationId) {
+        Applozic.ALApiService.sendMessage({
+            data: {
+                message: {
+                    'type': 5,
+                    'message': message,
+                    'to': userId,
+                    'conversationId': conversationId,
+                    'metadata': {},
+                    'source': 1
+                }
+            },
+            success: function (response) { console.log(response); },
+            error: function () {}
+        });
+    },
+    getTopicId: function (conversationId) {
+        Applozic.ALApiService.getTopicId({
+            data: {'conversationId': conversationId},
+            success: function (response) {
+                console.log(response);
+            },
+            error: function () {}
+        });
+    },
+    getConversationByTopic: function (topicId, tabId, clientGroupId) {
+        Applozic.ALApiService.fetchConversationByTopicId({
+            data: {
+                'topicId': topicId,
+                'tabId': tabId,
+                'pageSize': 60,
+                'clientGroupId': clientGroupId,
+                'isGroup': false
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function () {}
+        });
+    },
+    getMessagesByTopic: function (userId, conversationId, topicId, offset, limit) {
+        Applozic.ALApiService.getMessages({
+            data: {
+                userId: userId,
+                startIndex: offset,
+                pageSize: limit,
+                topicId: topicId,
+                conversationId: conversationId
+            },
+            success: function(response) {console.log(response);},
+            error: function() {}
+        });
+    },
+    getAllTopics: function (offset, limit) {
+        Applozic.ALApiService.getMessages({
+            data: {
+                startIndex: offset,
+                pageSize: limit
+            },
+            success: function(response) {console.log(response);},
+            error: function() {}
+        });
+    },
+    logout: function () {
+        window.Applozic.AlCustomService.logout();
+        window.Applozic.ALSocket.disconnect();
     }
 };
