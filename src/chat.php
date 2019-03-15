c2baf8be8c381e235ac9f7270a37ccf851d0c11b
Kaustubh Ekbote <k.ekbote@easternenterprise.com>
2019-03-15 14:17:51 +0530

    Change js name

--- src/chat.php
+++ src/chat.php
@@ -8,7 +8,7 @@ if (!empty($_POST['userName']) && !empty($_POST['password'])):
     <script type="text/javascript" src="../public/plugin/js/app/modules/applozic.jquery.js"></script>
     <script type="text/javascript" src="../public/plugin/js/app/fullview/applozic.fullview.js"></script>
     <script type="text/javascript" src="js/events.js"></script>
-    <script type="text/javascript" src="js/applozic-custom-ui.js"></script>
+    <script type="text/javascript" src="js/bas-applozic.js"></script>
     <script type="text/javascript">
         customApp.login('<?php echo $_POST['userName'];?>', '<?php echo $_POST['password'];?>', '<?php echo $_POST['userName'];?>', "{'title': 'Mercedes AMG GTC Coupe Edition 50', 'subtitle': 'Keramic Limited', 'link': 'https://www.bastrucks.com/photos/1280/Used-Car-Mercedes-AMG-GTC-Coupe-Edition-50-2018_136144_1.jpg?04102017', 'truckId': '70092900', 'registration': '2018'}");
     </script>
